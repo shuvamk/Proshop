1824ae3c7322071beeb72810255e9129bed98944
Shuvam Kumar <shuvamkumar@Shuvams-MacBook-Pro.local>
2021-07-28 23:52:01 +0530

    Created routes for products

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 import colors from "colors"; /* ts-import-sorter: disable */
 
@@ -14,14 +14,7 @@ app.get("/", (req, res) => {
   res.send("Api is runnin....");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
