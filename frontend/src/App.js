32e9028860b5973f7374381cf721dd77ca999e44
Shuvam Kumar <shuvamkumar@Shuvams-MacBook-Pro.local>
2021-07-28 18:42:30 +0530

    Added products and card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to proshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  Col,
+  Row,
+} from 'react-bootstrap';
+
+import Product from '../components/Product';
+import products from '../products';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Producst</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
