dffd1d97d813d0bf12e5e466a468aa57203f7858
Shuvam Kumar <shuvamkumar@Shuvams-MacBook-Pro.local>
2021-07-28 20:28:22 +0530

    Added dot env config

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -25,7 +25,7 @@ const ProductScreen = ({ match }) => {
       setProduct(data);
     };
     fetchProducts();
-  }, []);
+  }, [match]);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
