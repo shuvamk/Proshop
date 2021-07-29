3815e52975b3b78b1d7acdc1ef0932e844b4facb
Shuvam Kumar <shuvamkumar@Shuvams-MacBook-Pro.local>
2021-07-29 16:55:27 +0530

    Added product details page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,10 +6,14 @@ import {
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import { productListReducer } from './reducers/productReducers';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 const initialState = {};
 const middleware = [thunk];
